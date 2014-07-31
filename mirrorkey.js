2932e7f08c61d0f4867412b096183ba47c9accec
Jan Hancic <jan.hancic@gmail.com>
2014-07-31 20:54:36 +0100

    Add error checking

--- mirrorkey.js
+++ mirrorkey.js
@@ -1,2 +1,7 @@
 'use strict';
 
+module.exports = function(obj, config) {
+	if (obj === null || typeof obj !== 'object') {
+		throw 'The first argument to mirrorKey must be a object.';
+	}
+};
--- spec/mirrorkey_spec.js
+++ spec/mirrorkey_spec.js
@@ -1,5 +1,11 @@
+var mirrorKey = require('../mirrorkey');
+
 describe('MirrorKey', function() {
 	it('throws an exception if the passed in argument is not an object', function() {
-		expect(true).toBe(true);
+		var errorMsg = 'The first argument to mirrorKey must be a object.';
+
+		expect(function() {
+			mirrorKey(null);
+		}).toThrow(errorMsg);
 	});
 });
