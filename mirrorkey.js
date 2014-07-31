aded024abe37b8c0de72944c427b89d00fdacf2c
Jan Hancic <jan.hancic@gmail.com>
2014-07-31 20:58:21 +0100

    Add basic version of code, without config

--- mirrorkey.js
+++ mirrorkey.js
@@ -4,4 +4,16 @@ module.exports = function(obj, config) {
 	if (obj === null || typeof obj !== 'object') {
 		throw 'The first argument to mirrorKey must be a object.';
 	}
+
+	var result = {};
+
+	for (var key in obj) {
+		if (obj.hasOwnProperty(key) === false) {
+			continue;
+		}
+
+		result[key] = key;
+	}
+
+	return result;
 };
--- spec/mirrorkey_spec.js
+++ spec/mirrorkey_spec.js
@@ -8,4 +8,20 @@ describe('MirrorKey', function() {
 			mirrorKey(null);
 		}).toThrow(errorMsg);
 	});
+
+	it('returns an empty object if the argument is an empty object', function() {
+		var result = mirrorKey({});
+
+		expect(Object.keys(result).length).toEqual(0);
+	});
+
+	it('adds values to each key', function() {
+		var result = mirrorKey({
+			MY_KEY1: null,
+			MY_KEY2: null
+		});
+
+		expect(result.MY_KEY1).toEqual('MY_KEY1');
+		expect(result.MY_KEY2).toEqual('MY_KEY2');
+	});
 });
